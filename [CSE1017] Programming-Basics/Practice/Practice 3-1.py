eeddbfdaea5cee6ea755260868a3a4e9fa7a8710
StillnessFate <stillnessfate@gmail.com>
2017-03-28 00:01:05 +0900

    Commit

--- [CSE1017] Programming-Basics/Practice/Practice 3-1.py
+++ [CSE1017] Programming-Basics/Practice/Practice 3-1.py
@@ -1,1 +1,33 @@
-﻿#실습종료 후 공개됩니다.
+﻿# 정수 입력 확인 프로그램
+# 입력받은 값이 정수인지를 계산해주는 프로그램
+#
+# 입력: 임의의 숫자 - 정수만 허용
+# 출력: 입력받은 숫자
+#
+# 1. 정수를 입력받을 때까지 계속 입력받음
+# 2. 가장 앞에 '+'혹은 '-'기호가 있어도 정수로 취급
+# 3. 입력받은 값이 정수일 경우 입력받은 값을 출력하고 종료
+#
+# 작성자: 강민석
+# 작성날짜: 2017년 3월 23일 (version 1.0)
+
+def isNumber(str) :
+	result=False
+	try :
+		float(str)
+		result=2
+		int(str)
+		result=1
+	except :
+		pass
+
+	return result
+	
+def get_int_signed(message):
+	temp = input(message)
+	while not (isNumber(temp) == 1):
+		temp = input(message)
+		
+	return int(temp)
+
+print(get_int_signed("정수를 입력하시오\n"))
--- [CSE1017] Programming-Basics/Practice/Practice 3-3.py
+++ [CSE1017] Programming-Basics/Practice/Practice 3-3.py
@@ -1,1 +1,67 @@
-﻿#시험종료 후 공개됩니다.
+﻿# 안전한 제곱근 구하기 프로그램
+# 입력받은 값의 제곱근을 구하는 프로그램
+#
+# 입력: 임의의 숫자 - 유효한 정수 혹은 실수만 허용
+# 출력: 입력받은 숫자, 제곱근
+#
+# 1. 유효한 값을 입력받을 때까지 계속 입력받음
+# 2. 가장 앞에 '+'혹은 '-'기호가 있다면 유효하지 않은 값으로 취급
+# 3. 입력받은 값이 유효할 경우 입력된 값과 제곱근을 미리 주어진 양식에 맞추어 출력
+# 4. 출력을 한 이후에는 계속 진행할것인지를 묻고 답변에 따라 처리
+#
+# 작성자: 강민석
+# 작성날짜: 2017년 3월 23일 (version 1.0)
+
+import math
+
+def isNumber(str) :
+	result=False
+	try :
+		float(str)
+		result=2
+		int(str)
+		result=1
+	except :
+			pass
+
+	return result
+
+def print_result(num, sqrt):
+	print(num, "의 제곱근은", sqrt, "입니다.")
+
+def check_float(x) :
+	checkNumber = isNumber(x)
+	if 0 < checkNumber and x[0] != '+' and 0 <= float(x) :
+		return checkNumber
+	else :
+		return False
+	
+def stop() :
+	cont = input('계속하시겠습니까? (y/n) ')
+	while not (cont == 'y' or cont == 'n'):
+		cont = input('계속하시겠습니까? (y/n) ')
+	
+	return cont == 'n'
+
+def safe_sqrt():
+	print("제곱근을 구해드립니다.")
+	print("0이상의 수를 입력하세요.")
+	
+	while True :
+		temp = input("수를 입력하세요.\n")
+		check = check_float(temp)
+		if 0 < check :
+			if check == 1 :
+				num = int(temp)
+			else :
+				num = float(temp)
+			
+			sqrt = round(math.sqrt(num), 4)
+			print_result(num, sqrt)
+			
+			if stop() :
+				break
+	
+	print("안녕히 가세요.")
+	
+safe_sqrt()
