75dbb1cc08ecc65960cd9fb533717cb630b4e3f4
StillnessFate <stillnessfate@gmail.com>
2017-04-03 00:05:36 +0900

    Commit

---
+++ [CSE1017] Programming-Basics/Practice/Practice 4-1.py
@@ -0,0 +1,70 @@
+﻿# 최대공약수 계산 프로그램
+# 두 수의 최대공약수를 구하는 프로그램
+#
+# 출력: 두 수의 최대공약수와 계산 과정
+#
+# 1. 유클리드 알고리즘을 이용한 재귀함수로 최대공약수를 구함
+# 2. 이분 알고리즘을 이용한 꼬리재귀함수로 최대공약수를 구함
+# 3. 위 꼬리재귀 함수를 while문을 사용해 구현
+#
+# 작성자: 강민석
+# 작성날짜: 2017년 4월 2일 (version 1.0)
+
+def gcd1_1(m, n) :
+	if n != 0 :
+		print("gcd1_1", m, n)
+		return gcd1_1(n, m % n)
+	else:
+		return abs(m)
+	
+def gcd1_2(m, n) :
+	print("gcd1_2")
+	
+	def loop(m, n, k) :
+		print("gcd1_2, loop", m, n, k)
+		
+		if not (m == 0 or n == 0) :
+			if m % 2 == 0 and n % 2 == 0 :
+				return loop(m // 2, n // 2, k * 2)
+			elif m % 2 == 0 and n % 2 == 1 :
+				return loop(m // 2, n, k)
+			elif m % 2 == 1 and n % 2 == 0 :
+				return loop(m, n // 2, k)
+			elif m <= n :
+				return loop(m, (n - m) // 2, k)
+			else :
+				return loop(n, (m - n) // 2, k)
+		else :
+			if m == 0 :
+				return abs(n)#n * k
+			else :#n == 0
+				return abs(m * k)
+		
+	return loop(m, n, 1)
+
+def gcd1_3(m, n) :
+	print("gcd1_3")
+	
+	k = 1
+	while not (m == 0 or n == 0) :
+		print("gcd1_3, while", m, n, k)
+		
+		if m % 2 == 0 and n % 2 == 0 :
+			m, n, k = m // 2, n // 2, k * 2
+		elif m % 2 == 0 and n % 2 == 1 :
+			m = m // 2
+		elif m % 2 == 1 and n % 2 == 0 :
+			n = n // 2
+		elif m <= n :
+			n = (n - m) // 2
+		else:
+			m, n = n, abs((n - m) // 2)
+			
+	if m == 0 :
+		return abs(n)#n * k
+	else :#n==0
+		return abs(m * k)
+
+print(gcd1_1(18, 48))
+print(gcd1_2(18, 48))
+print(gcd1_3(18, 48))	
---
+++ [CSE1017] Programming-Basics/Practice/Practice 4-2.py
@@ -0,0 +1,150 @@
+﻿# 곱셈 계산 프로그램
+# 두 수의 곱을 구하는 프로그램
+#
+# 출력: 두 수의 곱과 계산 과정
+#
+# 1. 덧셈과 뺄셈 연산만을 이용해 꼬리재귀함수로 곱을 구함
+# 2. 위 꼬리재귀 함수를 while문을 사용해 구현
+# 3. double함수와 halve함수를 이용해 덧셈을 하는 횟수가 log n에비례하는 재귀함수로 곱을 구함
+# 4. 위 재귀 함수를 꼬리재귀 함수로 구현
+# 5. 위 꼬리재귀 함수를 while문을 사용해 구현
+# 6. 러시아 농부 곱셈 방법을 이용한 재귀함수로 곱을 구함
+# 7. 위 재귀 함수를 꼬리재귀 함수로 구현
+# 8. 위 꼬리재귀 함수를 while문을 사용해 구현
+#
+# 작성자: 강민석
+# 작성날짜: 2017년 4월 2일 (version 1.0)
+
+WRITE_CODE = False
+
+# 1. 곱셈 함수(꼬리 재귀)
+def mult1(m, n) :
+	def loop(n, ans) :
+		if 0 < n :
+			return loop(n - 1, ans + m)
+		else :
+			return ans
+		
+	return loop(n, 0)
+
+# 2. 곱셈 함수(while문)
+def mult2(m, n) :
+	ans = 0
+	while 0 < n :
+		ans += m
+		n -= 1
+		
+	return ans
+
+####################
+#      수정 X       #
+#  곱셈 함수 보조 함수  #
+def double(n) :    #
+	return n * 2   #
+                   #
+def halve(n) :     #
+	return n // 2  #
+####################
+
+# 3. 빠른 곱셈 함수(재귀)
+def fastmult1(m, n) :
+	if 0 < n :
+		if n % 2 == 0 :
+			return fastmult1(double(m), halve(n))
+		else :
+			return m + fastmult1(m, n - 1)
+	else :
+		return 0
+
+# 4. 빠른 곱셈 함수(꼬리 재귀)
+def fastmult2(m, n) :
+	def loop(m, n, ans) :
+		if 0 < n :
+			if n % 2 == 0 :
+				return loop(double(m), halve(n), ans)
+			else :
+				return loop(m, n-1, ans + m)
+		else :
+			return ans
+		
+	return loop(m, n, 0)
+
+# 5. 빠른 곱셈 함수(while문)=
+def fastmult3(m, n) :
+	ans = 0
+	while 0 < n :
+		if n % 2 == 0 :
+			m, n = double(m), halve(n)
+		else :
+			ans += m
+			n -= 1
+			
+	return ans
+
+# 6. 러시아 농부 곱셈 함수(재귀)
+def russianmult1(m, n) :
+	def loop(m, n) :
+		if 1 < n :
+			if n % 2 == 0 :#(m * (n % 2)) + loop(m * 2, n // 2)
+				return loop(m * 2, n // 2)
+			else :
+				return m + loop(m * 2, n // 2)
+		else :#n == 1
+			return m
+	
+	if 0 < n :
+		return loop(m, n)
+	else:
+		return 0
+
+# 7. 러시아 농부 곱셈 함수(꼬리 재귀)
+def russianmult2(m, n) :
+	def loop(m, n, ans) :
+		if 1 < n :
+			if n % 2 == 0 :#loop(m * 2, n // 2, ans + (m * (n % 2)))
+				return loop(m * 2, n // 2, ans)
+			else :
+				return loop(m * 2, n // 2, ans + m)
+		else :#n == 1
+			return ans + m
+		
+	if 0 < n :
+		return loop(m, n, 0)
+	else :
+		return 0
+
+# 8. 러시아 농부 곱셈 함수(while문)
+def russianmult3(m, n) :
+	ans = 0
+	while 1 < n :
+		if n % 2 == 1 :#ans += m * (n % 2)
+			ans += m
+		m, n = m * 2, n // 2
+	
+	return ans + m
+
+
+# 이 실행 부분은 이해하기 어려운 코드로 작성되어 있습니다.
+# 입력에 따라 알맞은 함수를 부르게 되어있습니다.
+# 채점을 위한 부분이니 그냥 넘어가세요
+####################################
+#              수정 X               #
+#           실행 스크립트              #
+funcMap = {                        #
+	"mult1": mult1,                #
+	"mult2": mult2,                #
+	"fastmult1": fastmult1,        #
+	"fastmult2": fastmult2,        #
+	"fastmult3": fastmult3,        #
+	"russianmult1": russianmult1,  #
+	"russianmult2": russianmult2,  #
+	"russianmult3": russianmult3,  #
+}                                  #
+function = input()                 #
+m = int(input())                   #
+n = int(input())                   #
+                                   #
+res = (funcMap.get(function))(m, n)#
+if type(res) == int :              #
+	print(res)                     #
+####################################
